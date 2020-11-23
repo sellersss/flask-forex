0b906f665c94d6b9687732a841f6402768b21d67
Sellers Crisp <sellershcrisp@gmail.com>
2020-11-22 19:56:48 -0500

    Methods to convert currency using the python-forex library

---
+++ conversion.py
@@ -0,0 +1,27 @@
+from forex_python.converter import CurrencyRates, CurrencyCodes
+
+"""Python Forex Methods"""
+cr = CurrencyRates()
+cc = CurrencyCodes()
+
+# print(cc.get_symbol("GBP"))
+
+
+def currency_code_check(currency_code):
+    """Get currency name from code"""
+
+    return cc.get_currency_name(currency_code) is not None
+
+
+def currency_rate(c_from, c_to, amt):
+    """Convert currency rate"""
+
+    res = round(cr.convert(c_from, c_to, amt), 2)
+    return res
+
+
+def currency_code(c_to):
+    """Get currency code"""
+
+    res = cc.get_symbol(c_to)
+    return res
